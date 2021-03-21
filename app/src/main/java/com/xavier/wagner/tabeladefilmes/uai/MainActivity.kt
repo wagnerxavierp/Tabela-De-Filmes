35da64b5678dc38bbab8e58ffff20ef5e32a85c8
Wagner Xavier <wagner3337@gmail.com>
2021-03-20 23:40:56 -0300

    20032021

--- app/src/main/java/com/xavier/wagner/tabeladefilmes/uai/MainActivity.kt
+++ app/src/main/java/com/xavier/wagner/tabeladefilmes/uai/MainActivity.kt
@@ -2,11 +2,35 @@ package com.xavier.wagner.tabeladefilmes.uai
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import com.xavier.wagner.tabeladefilmes.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupToolbar(toolbarMain, "Filmes", false)
+
     }
+
+    private fun setupToolbar(toolbar: Toolbar, title: String, navgationBack: Boolean){
+        toolbar.title = title
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(navgationBack)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.home -> {
+                this.onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
